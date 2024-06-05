acd8c2b5927d11fbb21f5612ee44b53986dff56f
hazminfirdaus <cmhazminfirdaus@gmail.com>
2024-06-05 17:28:08 +0200

    updated

--- customer/booking.php
+++ customer/booking.php
@@ -41,6 +41,10 @@
     <link rel="stylesheet" href="../css/animations.css">  
     <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="../img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
         
     <title>Sessions</title>
     <style>
